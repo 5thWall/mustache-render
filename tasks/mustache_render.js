dfbced965de0d1c47ece1a4e4f3c38b20330d207
Andy <5thWall@gmail.com>
2013-11-27 23:18:00 -0700

    Good enough documentation. Closes #12

--- tasks/mustache_render.js
+++ tasks/mustache_render.js
@@ -19,10 +19,22 @@ module.exports = function gruntTask(grunt) {
     clear_cache : false
   };
 
+  // Public: Create Object for rendering templates
+  //
+  // options - The Object options used to configure the renderer
+  //   directory - The String base directory to look for partials (default: "")
+  //   extension - The String extension for partials templates (default: ".mustache")
+  //   prefix    - The String common prefix for partials (default: "")
   function GMR(options) {
     this.options = options(DEFAULT_OPTIONS);
   }
 
+  // Public: Render a template with the given data to the given destination
+  //
+  // template - The String path to the template to be rendered
+  // data     - The String path to a JSON or YAML file
+  //            The data Object
+  // dest     - The String path to write the rendered template
   GMR.prototype.render = function render(data, template, dest) {
     var renderFn = this._compileTemplate(template);
     data = this._getData(data);
@@ -30,6 +42,7 @@ module.exports = function gruntTask(grunt) {
     grunt.file.write(dest, renderFn(data, this._getPartial.bind(this)));
   };
 
+  // Internal: Ensure data is the proper format.
   GMR.prototype._getData = function getData(data) {
     var datatype = typeof data;
 
@@ -45,6 +58,7 @@ module.exports = function gruntTask(grunt) {
     return data;
   };
 
+  // Internal: Read JSON or YAML data from file.
   GMR.prototype._getDataFromFile = function getDataFromFile(dataPath) {
     if (/\.json/i.test(dataPath)) {
       return grunt.file.readJSON(dataPath);
@@ -55,10 +69,12 @@ module.exports = function gruntTask(grunt) {
     grunt.fail.warn("Data file must be JSON or YAML. Given: " + dataPath);
   };
 
+  // Internal: Compile template to render function.
   GMR.prototype._compileTemplate = function compileTemplate(file) {
     return mustache.compile(grunt.file.read(file));
   };
 
+  // Internal: Retrieve String partial by name.
   GMR.prototype._getPartial = function getPartial(name) {
     var fileName = this.options.prefix + name + this.options.extension;
     var filePath = path.join(this.options.directory, fileName);
